026d3d6f63c5140730508491047b17d3a2a5de07
Daniel Jones <156261995+DanielAnthonyJones@users.noreply.github.com>
2024-03-16 18:04:59 +0000

    Add files via upload

---
+++ sentiment_analysis.py
@@ -0,0 +1,14 @@
+"""
+
+
+"""
+import spacy
+import pandas as pd
+import sys
+import os
+
+
+
+nlp = spacy.load("en_core_web_sm")
+
+amazon_products_df = pd.read_csv(os.path.join(sys.path[0],"amazon_product_reviews.csv")) 
